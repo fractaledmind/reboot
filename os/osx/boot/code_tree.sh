b328e783dee2eb0dde15ee57b83e7c1390b86a11
Stephen Margheim <margheim@sas.upenn.edu>
2014-12-29 22:42:28 -0600

    begin script to build ~/Code directory

---
+++ os/osx/boot/code_tree.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# ~/Code tree builder
+#
+set -e
+
+code="$HOME/Code"
+
+alfred_wf="$code/alfred_workflows"
+
+python_2="$code/python2_envs"
+
+python_3="$code/python3_envs"
